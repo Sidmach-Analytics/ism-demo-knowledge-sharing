5a2b7e0972ba1f4019f6cd2807b0cbca50b9e062
Ismail Alabi <alability99@gmail.com>
2021-07-15 14:22:40 +0100

    changed debug to false instead of true

--- DemoIsm/DemoIsm/settings.py
+++ DemoIsm/DemoIsm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qoi1oxzn(33mw35w22*@*!%!5@i9a&o@$tkf$pnt8$_6)tw)ev'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
